2094cccc9c415efba3308e473f50548825f295bc
Garvit Singla <garvits093@gmail.com>
2026-09-20 10:20:49 +0530

    Docs : Added function specifications

--- include/cli_parser.h
+++ include/cli_parser.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include "common.h"
 
+/**
+ * parse the command line arguments and set the hostname and resolvers
+ * @param argc the number of arguments
+ * @param argv the arguments
+ * @param hostname the hostname to resolve
+ * @param resolvers the resolvers to set
+ */
 void parse_cli(int argc, char* argv[],char *hostname,struct resolvers *resolvers);
 
 #endif
--- include/dns_resolver.h
+++ include/dns_resolver.h
@@ -7,8 +7,17 @@
 #include <arpa/inet.h>
 #include "common.h"
 
+/**
+ * get the default DNS resolver provided by os
+ * @return the ip address of the default DNS resolver
+ */
+char* get_default_dns_resolver (void);
 
-char* get_default_dns_resolver(void);
+/**
+ * set the primary DNS resolver for this query
+ * @param resolvers the resolvers to set
+ * @param server_addr the server address to set
+ */
 void set_default_dns_resolver (struct resolvers resolvers,struct sockaddr_in *server_addr);
 
 #endif
